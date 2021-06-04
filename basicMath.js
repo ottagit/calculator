ae466d7071d81c41398fd7cd2e0accb037f7d4d0
ottagit <ottacc66@gmail.com>
2021-06-04 11:18:58 +0300

    implement basic math operations

--- basicMath.js
+++ basicMath.js
@@ -21,3 +21,27 @@ function multiply(a,b) {
 function divide(a,b) {
   return a / b;
 }
+
+/* Create a new function operate that takes an operator
+   and 2 numbers and then calls one of the above
+   functions on the numbers
+*/
+function operate(operator, num1, num2) {
+  return operator(num1, num2);
+}
+
+// Implement multiply on the operate function
+let product = operate(multiply, 30, 2);
+console.log(product);
+
+// Implement add on the operate function
+let sum = operate(add, 30, 2);
+console.log(sum);
+
+// Implement subtract on the operate function
+let diff = operate(subtract, 30, 2);
+console.log(diff);
+
+// Implement divide on the operate function
+let div = operate(divide, 30, 2);
+console.log(div);
