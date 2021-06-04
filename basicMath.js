559e2ce97bbf5e4c3744fa8f1038c35819947cf3
ottagit <ottacc66@gmail.com>
2021-06-04 10:33:51 +0300

    define basic math operations

---
+++ basicMath.js
@@ -0,0 +1,23 @@
+// Create functions to mimic basic math operations
+// addition, multiplication, division and subtraction
+// All parameters are function-scoped -> their visibilty is limited within the functions
+
+// Addition
+function add(a,b) {
+  return a + b;
+}
+
+// Subtraction
+function subtract(a,b) {
+  return a - b;
+}
+
+// Multiplication
+function multiply(a,b) {
+  return a * b;
+}
+
+// Division
+function divide(a,b) {
+  return a / b;
+}
